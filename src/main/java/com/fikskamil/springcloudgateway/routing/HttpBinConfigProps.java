b646f40bf4af652729942069bd7fda0514e8171b
kamil.fiks <kamil.fiks@canpack.com>
2022-04-26 13:14:52 +0200

    Config to handle stubs in tests + basic routing tests

---
+++ src/main/java/com/fikskamil/springcloudgateway/routing/HttpBinConfigProps.java
@@ -0,0 +1,19 @@
+package com.fikskamil.springcloudgateway.routing;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "httpbin")
+public class HttpBinConfigProps {
+
+    private String httpbin = "http://httpbin.org:80";
+
+    public String getHttpbin() {
+        return httpbin;
+    }
+
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
+}
--- src/main/java/com/fikskamil/springcloudgateway/routing/Routing.java
+++ src/main/java/com/fikskamil/springcloudgateway/routing/Routing.java
@@ -1,27 +1,36 @@
 package com.fikskamil.springcloudgateway.routing;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HttpBinConfigProps.class)
 public class Routing {
+
+    private final HttpBinConfigProps httpBinConfigProps;
+
+    public Routing(HttpBinConfigProps httpBinConfigProps) {
+        this.httpBinConfigProps = httpBinConfigProps;
+    }
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        String httpUri = httpBinConfigProps.getHttpbin();
         return builder.routes()
                 .route(p -> p
                         .path("/get")
                         .filters(gatewayFilterSpec -> gatewayFilterSpec.
                                 addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
+                        .uri(httpUri))
                 .route(p -> p
                         .host("*.circuitbreaker.com")
                         .filters(f -> f.circuitBreaker(config -> config
                                 .setName("mycmd")
                                 .setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80"))
+                        .uri(httpUri))
                 .build();
     }
 }
