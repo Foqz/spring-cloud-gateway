222f8398d9d95713a2888064ddac99570f05fdaa
kamil.fiks <kamil.fiks@canpack.com>
2022-04-28 14:16:13 +0200

    Token is beeing validated

---
+++ src/main/java/com/fikskamil/springcloudgateway/webflux/CorsGlobalConfiguration.java
@@ -0,0 +1,18 @@
+package com.fikskamil.springcloudgateway.webflux;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
+    }
+}
