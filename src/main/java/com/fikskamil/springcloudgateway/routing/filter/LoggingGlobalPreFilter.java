ab621e39bb3b5a46b7d6acb6fe4a6c63b65dfa4c
kamil.fiks <kamil.fiks@canpack.com>
2022-05-09 12:59:35 +0200

    comment for enchanting request

--- src/main/java/com/fikskamil/springcloudgateway/routing/filter/LoggingGlobalPreFilter.java
+++ src/main/java/com/fikskamil/springcloudgateway/routing/filter/LoggingGlobalPreFilter.java
@@ -13,7 +13,7 @@ import reactor.core.publisher.Mono;
 public class LoggingGlobalPreFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("Pre Filter");
+        //could implement enchanting of request here
         return chain.filter(exchange)
                 .then(Mono.fromRunnable(() -> {
                     log.info("Hello from post filter");
